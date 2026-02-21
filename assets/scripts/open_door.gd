1cb62be2369b6330fd87301ee52545e890048870
Tomáš Adámek <ad.tomik@seznam.cz>
2026-02-22 00:16:57 +0100

    add follow path and add button game

--- assets/scripts/open_door.gd
+++ assets/scripts/open_door.gd
@@ -9,3 +9,4 @@ func _on_open_door() -> void:
 	if _tween: _tween.kill()
 	_tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 	_tween.tween_property(self, "position:x", position.x + slide_distance, slide_duration)
+	get_child(0).get_child(0).disabled = true
